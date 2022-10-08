85c855c005781ed5b7de086b74cc1c49aca54369
RuslanGareev <rudhamanyu@gmail.com>
2022-10-08 21:37:50 +0500

    Common for three

---
+++ Homework2/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+A (3,6,8); B (2,1,-7), -> 15.84
+
+A (7,-5, 0); B (1,-1,9) -> 11.53
+*/
+
+
+
+void DistanceBetweenPoints(int xa, int ya, int za, int xb, int yb, int zb)
+{
+    double distance = Math.Sqrt(Math.Pow((xb - xa), 2) + Math.Pow((yb - ya), 2) + Math.Pow((zb - za), 2));
+    Console.Write($"A ({xa},{ya},{za}); B ({xb},{yb},{zb}) -> {distance}");
+}
+
+DistanceBetweenPoints(7, -5, 0, 1, -1, 9);
---
+++ Homework3/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+*/
+
+void GetCubsUpToNum(int N)
+{
+    Console.Write($"{N} -> ");
+
+    for (int i = 1; i <= N; i++)
+    {
+        double cub = Math.Pow(i, 3);
+        Console.Write($"{cub} ");
+    }
+}
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+GetCubsUpToNum(number);
